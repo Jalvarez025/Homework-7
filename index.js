6af789b0e729cb9a8e9a1a0f40f6c049975f5705
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-10-14 19:46:26 -0500

    Not working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require('./develop/utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
